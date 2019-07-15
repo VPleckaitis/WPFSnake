a16cb0f508c2fb889b485e153cf6e4075415e8f9
Vytautas Pleckaitis <vytautas@email.com>
2019-07-15 21:21:45 +0100

    Initial commit

---
+++ WpfSnake/Models/CellModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSnake.Models
+{
+    public class Cell
+    {
+        private int _column, _row;
+        private CellTypeEnum _cellType;
+
+        public int Column
+        {
+            get { return _column; }
+            set { _column = value; }
+        }
+
+        public int Row
+        {
+            get { return _row; }
+            set { _column = value; }
+        }
+
+        public CellTypeEnum CellType
+        {
+            get { return _cellType; }
+            set { _cellType = value; }
+        }
+
+        public enum CellTypeEnum
+        {
+            EMPTY,
+            FOOD,
+            DIGESTED_FOOD,
+            SNAKE
+        }
+    }
+}
