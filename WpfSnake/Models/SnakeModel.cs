000a79ec0003a6f5b4b68efa835362f475c73f55
Vytautas Pleckaitis <vytautas@email.com>
2019-07-15 21:35:15 +0100

    Test travis commit

---
+++ WpfSnake/Models/SnakeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSnake.Models
+{
+    class Snake
+    {
+        private Cell SnakeHead;
+        List<Cell> SnakeBody = new List<Cell>();
+
+        public Snake(Cell startingCell)
+        {
+            SnakeHead = startingCell;
+            SnakeBody.Add(startingCell);
+        }
+    }
+}
