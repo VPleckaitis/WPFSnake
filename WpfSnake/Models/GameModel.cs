12aa4892c9458dc12ec611db2fd1e8e77a9ccc3d
Vytautas Pleckaitis <vytautas@email.com>
2019-07-18 21:54:49 +0100

    Small update

---
+++ WpfSnake/Models/GameModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSnake.Models
+{
+    public class Game
+    {
+        private Snake snake;
+        private Map map;
+        private MovementDirection mov = MovementDirection.None;
+        public MovementDirection Direction
+        {
+            get { return mov; }
+            set { mov = value; }
+        }
+
+        public enum MovementDirection
+        {
+            None,
+            Up,
+            Down,
+            Left,
+            Right
+        };
+  
+    }
+}
